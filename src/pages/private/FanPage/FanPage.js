95e5d383d763666e5627d149bd5838d344db35b0
imhan14 <quanghannguyen14@gmail.com>
2023-04-22 23:58:55 +0700

    .

--- src/pages/private/FanPage/FanPage.js
+++ src/pages/private/FanPage/FanPage.js
@@ -3,7 +3,7 @@ import ChatBox from "../Chat/ChatBox"
 import ChatList from "../Chat/ChatList/ChatList";
 import { ChatContext, ChatContextProvider } from "../Chat/context/ChatContext";
 export default function FanPage() {
-  return <div>This is fanpage
+  return <div>
     <ChatContextProvider>
       <ChatList/>
     </ChatContextProvider>
