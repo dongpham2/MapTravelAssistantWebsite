62851b331887ff3d50a923ad8473364725c4ca85
Ngo Quoc Hieu <hieu.ngo@dssolution.vn>
2023-04-23 10:17:20 +0700

    push code

--- src/pages/public/Home/Map/index.js
+++ src/pages/public/Home/Map/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Map.module.scss";
-import googleMapReact from "google-map-react";
+
+import L from 'leaflet'
+import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const cx = classNames.bind(styles);
+const markerIc = new L.Icon({
+  iconUrl: require("../../../../assets/images/icon/iconmap.png"),
+  iconSize: [35, 45]
+})
 
 export default function Map() {
-  const coordinates = { lat: 0, lng: 0 };
+  const position = [16.06089773991539, 108.22178340647243]
   return (
-    <div className={cx("wrapper")}>
-      <googleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={(50, 50, 50, 50)}
-        options={""}
-        onChange={""}
-        onChildClick={""}
-      ></googleMapReact>
-    </div>
-  );
+    <MapContainer center={position} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/*  <LeafletGeocoder /> */}
+        {/* <LeafletRoutingMachine /> */}
+        <Marker position={[16.06089773991539, 108.22178340647243]} icon={markerIc}>
+          <Popup >
+          <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    )
 }
